ed3f90ab8961e3fe4a7c767cb0ae4f601956fa44
Rafael <you@example.com>
2023-07-15 21:52:56 -0300

    first commit

---
+++ dono/gitdobot.js
@@ -0,0 +1,5 @@
+const gitdobot = (prefix) => {
+return`*Oiiiiêê, esta versão minha só está disponível na bronxys> https://bronxyshost.com.br o melhor site de hospedagem de bots/sites da América latina. nao perca tempo, yenha agora os melhores bots super atualizados do mundoooo*🤩🤩🤩`
+}
+
+exports.gitdobot = gitdobot
